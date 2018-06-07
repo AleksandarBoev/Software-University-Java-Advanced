1656b8fe3879de0c6db336eadbc1fa5aada2e8bd
AleksandarBoev <aleksandarboev95@gmail.com>
2018-06-07 22:21:07 +0300

    Added ExamPrep1 project wihout solutions

---
+++ ExamPreparation1/src/P01Shockwave.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P01Shockwave {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Hello");
+
+        //main ends here
+    }
+}
